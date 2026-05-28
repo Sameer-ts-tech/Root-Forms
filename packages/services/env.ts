102928cf55b0d22f1f405dca66eb4f58b8d7bc35
Sameer-ts-tech <sameer.ts.tech@gmail.com>
2026-05-28 16:12:14 +0530

    console statements add to see the deployment issue

--- packages/services/env.ts
+++ packages/services/env.ts
@@ -6,7 +6,12 @@ const envSchema = z.object({
 
 function createEnv(env: NodeJS.ProcessEnv) {
     const safeParseResult = envSchema.safeParse(env);
-    if (!safeParseResult.success) throw new Error(safeParseResult.error.message);
+    if (!safeParseResult.success) {
+        console.error("ENVIRONMENT VARIABLE ERROR:");
+        console.error(JSON.stringify(safeParseResult.error.issues, null, 2));
+        console.error("Available ENV KEYS:", Object.keys(process.env).join(", "));
+        throw new Error(safeParseResult.error.message);
+    }
     return safeParseResult.data;
 }
 
